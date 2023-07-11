e1d444a4dd422920f7d5ed5f5e598d5e264b3fa5
XBigTK13X <xbigtk13x@gmail.com>
2023-07-10 21:37:22 -0500

    Version bump

--- android-client/src/com/simplepathstudios/pbr/PBRSettings.java
+++ android-client/src/com/simplepathstudios/pbr/PBRSettings.java
@@ -4,8 +4,8 @@ package com.simplepathstudios.pbr;
 import android.net.Uri;
 
 public class PBRSettings {
-    public static final String BuildDate = "July 09, 2023";
-    public static final String ClientVersion = "1.0.3";
+    public static final String BuildDate = "July 10, 2023";
+    public static final String ClientVersion = "1.0.4";
     public static boolean EnableDebugLog = false;
     public static Uri UpdatePBRUrl = Uri.parse("http://9914.us/software/android/picture-book-reader.apk");
     public static boolean DebugResourceLeaks = false;
